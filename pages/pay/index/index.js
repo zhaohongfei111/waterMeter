31e939619c04f3935f4e081faef5dc08a57fbb2d
zhaohongfei111 <1084842688@qq.com>
2019-09-22 22:39:26 +0800

    页面添加轮播图

--- pages/pay/index/index.js
+++ pages/pay/index/index.js
@@ -5,9 +5,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
   },
-
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
